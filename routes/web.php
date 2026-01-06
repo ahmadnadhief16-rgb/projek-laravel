b2ab4e8acb68ad77643fbc1435f4ffebd4e2f2c7
homenadhief <ahmadnadhief.16@gmail.com>
2026-01-06 15:14:40 +0700

    Add routes and session logic

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,35 @@
 <?php
 
-use App\Http\Controllers\SantriController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('beranda');
+    return view('home');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
 });
 
 Route::get('/tata-tertib', function () {
     return view('tata-tertib');
 });
 
-Route::get('/pendaftaran', [SantriController::class, 'create']);
-Route::post('/pendaftaran', [SantriController::class, 'store']);
+Route::get('/pendaftaran', function () {
+    return view('pendaftaran');
+});
 
-Route::get('/santri', [SantriController::class, 'index']);
+Route::post('/pendaftaran', function (Request $request) {
+    $request->session()->put('santri', [
+        'nama' => $request->nama,
+        'alamat' => $request->alamat,
+        'asal_sekolah' => $request->asal_sekolah
+    ]);
+
+    return redirect('/hasil');
+});
+
+Route::get('/hasil', function (Request $request) {
+    $santri = $request->session()->get('santri');
+    return view('hasil', compact('santri'));
+});
