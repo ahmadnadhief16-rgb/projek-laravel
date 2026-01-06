88179c3a79002403853dea7bb521c9c9f9ccef18
homenadhief <ahmadnadhief.16@gmail.com>
2026-01-06 13:44:00 +0700

    Add routes and session logic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/tata-tertib', function () {
+    return view('tata-tertib');
+});
+
+Route::get('/pendaftaran', function () {
+    return view('pendaftaran');
+});
+
+Route::post('/pendaftaran', function (Request $request) {
+    $request->session()->put('santri', [
+        'nama' => $request->nama,
+        'alamat' => $request->alamat,
+        'asal_sekolah' => $request->asal_sekolah
+    ]);
+
+    return redirect('/hasil');
+});
+
+Route::get('/hasil', function (Request $request) {
+    $santri = $request->session()->get('santri');
+    return view('hasil', compact('santri'));
 });
